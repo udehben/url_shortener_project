3ea1e9e9f0fa7fcaf249ad4f2dc41743890848d8
Ben <udehbudeh@gmail.com>
2022-08-29 11:55:20 +0100

    created an admin endpoint

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -2,10 +2,12 @@ import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
 
+from starlette.datastructures import URL
 from sqlalchemy.orm import Session
 
 from . import models, schemas, crud
 from .database import SessionLocal, engine
+from .config import get_settings
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -24,6 +26,17 @@ def raise_not_found(request):
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        'administration info', secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return (db_url)
+
+
+
 
 @app.get('/')
 def read_root():
@@ -35,9 +48,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
         raise_bad_request(message="Your provided URL is not valid")
 
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-    return (db_url)
+    return get_admin_info(db_url)
 
 @app.get('/{url_key}')
 def forward_to_target_url(
@@ -47,5 +58,18 @@ def forward_to_target_url(
 ):
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+@app.get(
+    '/admin/{secret_key}',
+    name = 'administration info',
+    response_model=schemas.URLInfo,
+)
+def get_url_info(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
